7d981b92a34916b3927cc6f2cdd5b410d03a0fac
Md Masud Sk <masudsk7074@gmail.com>
2026-06-29 17:05:08 +0530

    Added solution - LeetHub

--- 704723-subset-sums-GFG/704723-subset-sums.cpp
+++ 704723-subset-sums-GFG/704723-subset-sums.cpp
@@ -1,26 +1,21 @@
 class Solution {
-  public:
-  void subset_sum(vector<int>&nums,int index,int n,vector<int> &temp,vector<int> &result){
-      if(index==n){
-          int sum=0;
-          for(int val:temp){
-              sum+=val;
-          }
-          result.push_back(sum);
-          return;
-      }
-      subset_sum(nums,index+1,n,temp,result);
-      temp.push_back(nums[index]);
-      subset_sum(nums,index+1,n,temp,result);
-      temp.pop_back();
-  }
-    vector<int> subsetSums(vector<int>& arr) {
-        // code here
-        vector<int> temp,result;
-        subset_sum(arr,0,arr.size(),temp,result);
-        return result;
-    }
+	public:
+	void subset_sum(vector<int>&nums, int index, int sum, vector<int> &result) {
+		if (index == -1) {
+			result.push_back(sum);
+			return;
+		}
+		subset_sum(nums, index - 1, sum, result);
+		subset_sum(nums, index - 1, sum + nums[index], result);
+	}
+	vector<int> subsetSums(vector<int>& arr) {
+		// code here
+		vector<int> result;
+		subset_sum(arr, arr.size() - 1, 0, result);
+		return result;
+	}
 };
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
