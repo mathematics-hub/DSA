171e3670c158a4d9d6384389358af72a536d9869
Md Masud Sk <masudsk7074@gmail.com>
2026-07-05 17:05:33 +0530

    Added solution - LeetHub

--- 702915-single-number-GFG/702915-single-number.cpp
+++ 702915-single-number-GFG/702915-single-number.cpp
@@ -7,16 +7,12 @@ class Solution {
 		if (n == 1) {
 			return arr[0];
 		}
-		for (int i = 1; i<n - 1; i++) {
-			if (arr[i - 1]<arr[i] && arr[i]<arr[i + 1]) {
+		for (int i = 0; i<n - 1; i += 2) {
+			if (arr[i] != arr[i + 1]) {
 				return arr[i];
 			}
 		}
-		if (arr[0] != arr[1]) {
-			return arr[0];
-		}
-		else
-			return arr[n - 1];
+		return arr[n - 1];
 	}
 };
 
