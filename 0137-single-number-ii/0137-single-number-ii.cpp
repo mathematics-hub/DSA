24054c7a12988f8741b96eb321e352fc966c4fcd
Md Masud Sk <masudsk7074@gmail.com>
2026-07-05 16:22:21 +0530

    Time: 0 ms (100%), Space: 13.3 MB (64.14%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int result = 0;
+        for (int bit_no = 0; bit_no < 32; bit_no++) {
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                if (nums[i] & (1 << bit_no)) {
+                    sum++;
+                }
+            }
+            if (sum % 3 != 0) {
+                result = result | (1 << bit_no);
+            }
+        }
+        return result;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
