9fa375654815ea00031d42e09abfd4ab0f9e2dc5
Md Masud Sk <masudsk7074@gmail.com>
2026-07-05 17:04:19 +0530

    Added solution - LeetHub

---
+++ 702915-single-number-GFG/702915-single-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+	public:
+	int getSingle(vector<int>& arr) {
+		// code here
+		sort(arr.begin(), arr.end());
+		int n = arr.size();
+		if (n == 1) {
+			return arr[0];
+		}
+		for (int i = 1; i<n - 1; i++) {
+			if (arr[i - 1]<arr[i] && arr[i]<arr[i + 1]) {
+				return arr[i];
+			}
+		}
+		if (arr[0] != arr[1]) {
+			return arr[0];
+		}
+		else
+			return arr[n - 1];
+	}
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
