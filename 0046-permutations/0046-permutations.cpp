0aa8a40ccce61ff455fdd23d592dd0563f157b55
Md Masud Sk <masudsk7074@gmail.com>
2026-07-06 11:31:15 +0530

    Time: 0 ms (100%), Space: 10.5 MB (88.48%) - LeetHub - Time: O(n!), Space: O(n)

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    void permutation(vector<int>& nums, vector<int>& temp,
-                     vector<bool>& visited, vector<vector<int>>& result) {
-        if (nums.size() == temp.size()) {
-            result.push_back(temp);
-            return;
+    void permutation(vector<int>& nums, int index,
+                     vector<vector<int>>& result) {
+        if (index == nums.size()) {
+            result.push_back(nums);
         }
-        for (int i = 0; i < visited.size(); i++) {
-            if (visited[i] == 0) {
-                visited[i] = 1;
-                temp.push_back(nums[i]);
-                permutation(nums, temp, visited, result);
-                visited[i] = 0;
-                temp.pop_back();
-            }
+        for (int i = index; i < nums.size(); i++) {
+            swap(nums[index], nums[i]);
+            permutation(nums, index + 1, result);
+            swap(nums[index], nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> temp;
-        vector<bool> visited(nums.size(), 0);
+        int index = 0;
         vector<vector<int>> result;
-        permutation(nums, temp, visited, result);
+        permutation(nums, index, result);
         return result;
     }
 };
