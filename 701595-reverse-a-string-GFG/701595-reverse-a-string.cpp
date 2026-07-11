e417d6d93e2c23841bb15003a42a282362a6237b
Md Masud Sk <masudsk7074@gmail.com>
2026-07-11 15:08:35 +0530

    Added solution - LeetHub

--- 701595-reverse-a-string-GFG/701595-reverse-a-string.cpp
+++ 701595-reverse-a-string-GFG/701595-reverse-a-string.cpp
@@ -3,8 +3,11 @@ class Solution {
 	string reverseString(string& s) {
 		// code here
 		int n = s.size();
-		for (int i = 0; i<n/2; i++) {
-			swap(s[i], s[n - 1-i]);
+		int start=0,end=n-1;
+		while(start<end){
+		    swap(s[start],s[end]);
+		    start++;
+		    end--;
 		}
 		return s;
 	}
