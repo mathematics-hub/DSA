a6604df7f5a51b42a247aad6a9dea59268b86c7e
Md Masud Sk <masudsk7074@gmail.com>
2026-07-12 11:22:14 +0530

    Added solution - LeetHub

--- 701190-tower-of-hanoi-GFG/701190-tower-of-hanoi.cpp
+++ 701190-tower-of-hanoi-GFG/701190-tower-of-hanoi.cpp
@@ -3,7 +3,7 @@ class Solution {
 	int towerOfHanoi(int n, int from, int to, int aux) {
 		// code here
 		if (n == 0)return 0;
-		return towerOfHanoi(n - 1, from, aux, to) + 1+ towerOfHanoi(n - 1, aux, to, from);
+		return (1 << n) - 1;
 	}
 };
 
