027ea9273b5b7e4527d2d50ee8a551c25690d015
Md Masud Sk <masudsk7074@gmail.com>
2026-07-12 15:40:04 +0530

    Time: 87 ms (5.02%), Space: 7.9 MB (65.27%) - LeetHub - Time: O(n^2), Space: O(n)

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
+    int winner(vector<bool>& person, int index, int n, int k, int lp) {
+        if (lp == 1) {
+            return index + 1;
+        }
+        int count = 0;
+        while (true) {
+            if (person[index] == 0) {
+                count++;
+            }
+            if (count == k) {
+                break;
+            }
+            index = (index + 1) % n;
+        }
+        person[index] = 1;
+        while (person[index] == 1) {
+            index = (index + 1) % n;
+        }
+        return winner(person, index, n, k, lp - 1);
+    }
     int findTheWinner(int n, int k) {
         vector<bool> present(n, 0);
         int leftPerson = n, index = 0;
-        while (leftPerson > 1) {
-            int count = 0;
-            while (true) {
-                if (present[index] == 0) {
-                    count++;
-                }
-                if (count == k) {
-                    break;
-                }
-                index = (index + 1) % n;
-            }
-            present[index] = 1;
-            leftPerson--;
-            while (present[index] == 1) {
-                index = (index + 1) % n;
-            }
-        }
-        return index + 1;
+        return winner(present,index,n,k,leftPerson);
     }
 };
 
