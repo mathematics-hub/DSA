da26752410387343cc9c6fb75dd99e67e0b3540c
Md Masud Sk <masudsk7074@gmail.com>
2026-07-24 17:41:48 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 703776-decision-making-GFG/703776-decision-making.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin>> n>> m;
+	
+	// code here
+	if (n<m)cout << "less";
+	else if (n == m)cout << "equal";
+	else
+		cout << "greater";
+	return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
