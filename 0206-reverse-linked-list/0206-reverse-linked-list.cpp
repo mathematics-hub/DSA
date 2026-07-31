3136abac1c57c2200475f5f49179f3fc763fd0b0
Md Masud Sk <masudsk7074@gmail.com>
2026-07-31 18:35:03 +0530

    Time: 0 ms (100%), Space: 13.4 MB (72.87%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp = head;
+        vector<int> store;
+        while (temp != NULL) {
+            store.push_back(temp->val);
+            temp = temp->next;
+        }
+        temp = head;
+        while (temp != NULL) {
+            temp->val = store.back();
+            store.pop_back();
+            temp = temp->next;
+        }
+        return head;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
