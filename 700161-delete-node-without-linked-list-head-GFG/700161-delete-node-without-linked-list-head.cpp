d18d1b4fcdcb1f1a81be74a3211799a8128f4c59
Md Masud Sk <masudsk7074@gmail.com>
2026-07-31 18:15:11 +0530

    Added solution

---
+++ 700161-delete-node-without-linked-list-head-GFG/700161-delete-node-without-linked-list-head.cpp
@@ -0,0 +1,26 @@
+/* Structure of Linked List Node
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    void deleteNode(Node* curr) {
+        // code here
+        Node *temp=curr->next;
+        curr->data=temp->data;
+        curr->next=temp->next;
+        delete temp;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
