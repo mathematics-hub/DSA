4d88e83cb02918d27c07aad6c1caf6674b10e72a
Md Masud Sk <masudsk7074@gmail.com>
2026-08-05 13:04:24 +0530

    Added solution

--- 700332-remove-cycle-in-linked-list-GFG/700332-remove-cycle-in-linked-list.cpp
+++ 700332-remove-cycle-in-linked-list-GFG/700332-remove-cycle-in-linked-list.cpp
@@ -14,16 +14,26 @@ class Solution {
 	public:
 	void removeLoop(Node* head) {
 		// code here
-		unordered_map<Node *, bool> visited;
-		Node *curr = head;
-		while (curr != NULL) {
-			if (visited[curr->next] == 1) {
-				curr->next = NULL;
-			//	break;
+		Node *slow = head, *fast = head;
+		while (fast != NULL && fast->next != NULL) {
+			slow = slow->next;
+			fast = fast->next->next;
+			if (slow == fast) {
+				break;
 			}
-			visited[curr] = 1;
-			curr = curr->next;
 		}
+		if (fast == NULL || fast->next == NULL) {
+			return;
+		}
+		slow = head;
+		while (slow != fast) {
+			slow = slow->next;
+			fast = fast->next;
+		}
+		while (slow->next != fast) {
+			slow = slow->next;
+		}
+		slow->next = NULL;
 	}
 };
 
