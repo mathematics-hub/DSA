2ffef043b023b9d5c67c50803ae35f87c9f8d9cd
Md Masud Sk <masudsk7074@gmail.com>
2026-08-07 21:19:35 +0530

    Time: 116 ms (6.48%), Space: 18.6 MB (48.2%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge(ListNode* head1, ListNode* head2) {
+        ListNode *head = new ListNode(0), *tail = head;
+        while (head1 && head2) {
+            if (head1->val <= head2->val) {
+                tail->next = head1;
+                head1 = head1->next;
+            } else {
+                tail->next = head2;
+                head2 = head2->next;
+            }
+            tail = tail->next;
+            tail->next = NULL;
+        }
+        if (head1) {
+            tail->next = head1;
+        } else {
+            tail->next = head2;
+        }
+        return head->next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+       if (lists.empty())
+            return NULL;
+        ListNode* head = lists[0];
+        for (int i = 1; i < lists.size(); i++) {
+            head = merge(head, lists[i]);
+        }
+        return head;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
