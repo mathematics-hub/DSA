d44e2d443f61240cdd37e41d03505b04391d608b
Md Masud Sk <masudsk7074@gmail.com>
2026-08-07 15:32:51 +0530

    Added solution

---
+++ 700005-reverse-a-linked-list-GFG/700005-reverse-a-linked-list.cpp
@@ -0,0 +1,34 @@
+/* Structure of Linked List Node
+class Node {
+	public:
+	int data ;
+	Node *next ;
+	
+	Node(int x) {
+		data = x ;
+		next = nullptr ;
+	}
+};
+*/
+
+class Solution {
+	public:
+	Node *reverse(Node *curr, Node *prev) {
+		if (curr == NULL) {
+			return prev;
+		}
+		Node *temp = curr->next;
+		curr->next = prev;
+		return reverse(temp, curr);
+	}
+	Node* reverseList(Node* head) {
+		// code here
+		head = reverse(head, NULL);
+		return head;
+	}
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
