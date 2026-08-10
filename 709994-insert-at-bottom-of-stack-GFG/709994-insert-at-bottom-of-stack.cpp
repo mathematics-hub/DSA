499ddc3245d5cdaa8e9985bd44b405107203693a
Md Masud Sk <masudsk7074@gmail.com>
2026-08-10 14:13:41 +0530

    Added solution

---
+++ 709994-insert-at-bottom-of-stack-GFG/709994-insert-at-bottom-of-stack.cpp
@@ -0,0 +1,22 @@
+class Solution {
+	public:
+	stack<int> insertAtBottom(stack<int> &st, int x) {
+		// code here
+		stack<int> temp;
+		while (!st.empty()) {
+			temp.push(st.top());
+			st.pop();
+		}
+		st.push(x);
+		while (!temp.empty()) {
+			st.push(temp.top());
+			temp.pop();
+		}
+		return st;
+	}
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
