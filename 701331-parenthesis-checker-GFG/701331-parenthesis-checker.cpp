f704c2c35dc47b0745f57395512ba07e60b39f74
Md Masud Sk <masudsk7074@gmail.com>
2026-08-10 16:26:31 +0530

    Added solution

--- 701331-parenthesis-checker-GFG/701331-parenthesis-checker.cpp
+++ 701331-parenthesis-checker-GFG/701331-parenthesis-checker.cpp
@@ -4,34 +4,22 @@ class Solution {
 		// code here
 		stack<char> temp;
 		for (int i = 0; i<arr.size(); i++) {
-			if (temp.empty()) {
-				if (arr[i] == '(' || arr[i] == '{' || arr[i] == '[') {
-					temp.push(arr[i]);
-				}
-				else {
-					return 0;
-				}
-			}
-			else if (arr[i] == '(' || arr[i] == '{' || arr[i] == '[') {
+			if (arr[i] == '(' || arr[i] == '{' || arr[i] == '[') {
 				temp.push(arr[i]);
 			}
 			else {
-				if ((temp.top() == '(' && arr[i] == ')') ||
-				(temp.top() == '{' && arr[i] == '}') || (temp.top() == '[' && arr[i] == ']'))
+				if (!temp.empty() && ((temp.top() == '(' && arr[i] == ')') ||
+				(temp.top() == '{' && arr[i] == '}') ||
+				(temp.top() == '[' && arr[i] == ']')))
 				{
 					temp.pop();
 				}
 				else {
-					return 0;
+					return false;
 				}
 			}
 		}
-		if (temp.size() == 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
+		return temp.empty();
 	}
 };
 
