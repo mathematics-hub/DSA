4355c68b5779a8959bd41ff7ae288e731372457b
Md Masud Sk <masudsk7074@gmail.com>
2026-08-12 15:51:42 +0530

    Time: 162 ms (5%), Space: 27.9 MB (95.65%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> ans(size);
+        for (int i = 0; i < size; i++) {
+            int j = (i + 1) % size;
+            while (1) {
+                if (j == i) {
+                    ans[i] = -1;
+                    break;
+                }
+                if (nums[j] > nums[i]) {
+                    ans[i] = nums[j];
+                    break;
+                }
+                j = (j + 1) % size;
+            }
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
