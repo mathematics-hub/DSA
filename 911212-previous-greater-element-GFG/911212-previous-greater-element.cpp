45f50b4a27ffd74dfb61867c9f6df72094cc2dd0
Md Masud Sk <masudsk7074@gmail.com>
2026-08-13 12:27:37 +0530

    Added solution

---
+++ 911212-previous-greater-element-GFG/911212-previous-greater-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+	public:
+	vector<int> preGreaterEle(vector<int>& arr) {
+		//  code here
+		stack<int> temp;
+		vector<int> ans(arr.size(), -1);
+		for (int i = arr.size() - 1; i >= 0; i--) {
+			if (temp.empty()) {
+				temp.push(i);
+			}
+			else {
+				while (!temp.empty() && arr[temp.top()]<arr[i]) {
+					ans[temp.top()] = arr[i];
+					temp.pop();
+				}
+				temp.push(i);
+			}
+		}
+		return ans;
+	}
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
