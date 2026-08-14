e2b6d21aac8bbf21fad4b4549e75dc703993d550
Md Masud Sk <masudsk7074@gmail.com>
2026-08-14 15:37:41 +0530

    Added solution

--- 700253-the-celebrity-problem-GFG/700253-the-celebrity-problem.cpp
+++ 700253-the-celebrity-problem-GFG/700253-the-celebrity-problem.cpp
@@ -3,33 +3,52 @@ class Solution {
 	int celebrity(vector<vector<int>> & mat) {
 		// code here
 		int n = mat.size();
-		for (int col = 0; col<n; col++) {// check every column
-			// every one know him
-			bool flag = true;
-			for (int row = 0; row<n; row++) {// check every row
-				if (mat[row][col] == 0) {
-					flag = false;
-					break;
-				}
-			}
-			if (flag == false) {
+		stack<int> temp;
+		for (int i = 0; i<n; i++) {
+			temp.push(i);
+		}
+		while (!temp.empty() && temp.size() != 1) {
+			int i = temp.top();
+			temp.pop();
+			int j = temp.top();
+			temp.pop();
+			if (mat[i][j] == 1 && mat[j][i] == 1) {
 				continue;
 			}
-			// check col no row
-			// where every body unknown
-			for (int j = 0; j<n; j++) {
-				if (j != col && mat[col][j] == 1) {
-					flag = false;
-					break;
-				}
+			else if (mat[i][j] == 0 && mat[j][i] == 0) {
+				continue;
 			}
-			if (flag) {
-				return col;
+			else if (mat[i][j] == 1 && mat[j][i] == 0) {
+				temp.push(j);
 			}
+			else if (mat[i][j] == 0 && mat[j][i] == 1) {
+				temp.push(i);
+			}
+		}
+		if (temp.empty()) {
+			return - 1;
+		}
+		int index = temp.top();
+		bool flag = true;
+		for (int j = 0; j<n; j++) {
+			if (index != j && mat[index][j] == 1) {
+				flag = false;
+				break;
+			}
+		}
+		for (int i = 0; i<n; i++) {
+			if (mat[i][index] == 0) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag == true) {
+			return index;
 		}
 		return - 1;
 	}
 };
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
