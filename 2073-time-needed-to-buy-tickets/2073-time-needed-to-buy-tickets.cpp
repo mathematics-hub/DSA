1c31ea80eda234f3240bc682368ebdf78bb40e11
Md Masud Sk <masudsk7074@gmail.com>
2026-08-18 18:28:07 +0530

    tc=O(n)

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int t = tickets[k];
         int time = 0;
-        queue<int> q;
         for (int i = 0; i < tickets.size(); i++) {
-            q.push(i);
-        }
-        while (tickets[k] != 0) {
-            tickets[q.front()]--;
-            time++;
-            if (tickets[q.front()] != 0) {
-                q.push(q.front());
+            if (i <= k) {
+                {
+                    time += min(tickets[i], t);
+                }
+            } else {
+                time += min(tickets[i], t - 1);
             }
-            q.pop();
         }
         return time;
     }
