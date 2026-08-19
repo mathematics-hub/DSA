01c41848fd711938e6fa275756a6f8262d1a427e
Md Masud Sk <masudsk7074@gmail.com>
2026-08-19 15:05:30 +0530

    Added solution

---
+++ 703375-first-negative-in-windows-of-size-k-GFG/703375-first-negative-in-windows-of-size-k.cpp
@@ -0,0 +1,39 @@
+class Solution {
+	public:
+	vector<int> firstNegInt(vector<int>& arr, int k) {
+		// code here
+		queue<int> all;
+		queue<int> neg;
+		vector<int> ans;
+		for (int i = 0; i < k - 1; i++)
+			{
+			if (arr[i]<0) {
+				neg.push(arr[i]);
+			}
+			all.push(arr[i]);
+		}
+		for (int i = k - 1; i < arr.size(); i++)
+			{
+			if (arr[i]<0) {
+				neg.push(arr[i]);
+			}
+			all.push(arr[i]);
+			if (neg.empty()) {
+				ans.push_back(0);
+			}
+			else {
+				ans.push_back(neg.front());
+			}
+			if (!neg.empty() && all.front() == neg.front()) {
+				neg.pop();
+			}
+			all.pop();
+		}
+		return ans;
+	}
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
