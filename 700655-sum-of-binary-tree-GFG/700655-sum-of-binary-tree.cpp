da66a0183c713206437a7080435d0e5b587e30e1
Md Masud Sk <masudsk7074@gmail.com>
2026-08-23 16:03:18 +0530

    recursion

---
+++ 700655-sum-of-binary-tree-GFG/700655-sum-of-binary-tree.cpp
@@ -0,0 +1,28 @@
+/* Tree Node Structure
+class Node {
+	public:
+	int data;
+	Node* left;
+	Node* right;
+	Node(int val) {
+		data = val;
+		left = right = nullptr;
+	}
+}; */
+
+class Solution {
+	public:
+	int sum
+	int sumBT(Node* root) {
+		// code here
+		if (!root) {
+			return 0;
+		}
+		return sumBT(root->left) + sumBT(root->right) + (root->data);
+	}
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
