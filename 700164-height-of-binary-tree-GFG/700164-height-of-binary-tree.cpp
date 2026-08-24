31557169b4f02b89a5fe1ec8483d64b745a8fd6b
Md Masud Sk <masudsk7074@gmail.com>
2026-08-24 11:48:39 +0530

    Added solution

---
+++ 700164-height-of-binary-tree-GFG/700164-height-of-binary-tree.cpp
@@ -0,0 +1,27 @@
+/* Structrue of Binary Tree Node
+class Node {
+	public:
+	int data;
+	Node* left;
+	Node* right;
+	
+	Node(int val) {
+		data = val;
+		left = right = nullptr;
+	}
+}; */
+
+class Solution {
+	public:
+	int height(Node* root) {
+		// code here
+		if (root == NULL) {
+			return -1;
+		}
+		return 1 + max(height(root->left), height(root->right));
+	}
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
