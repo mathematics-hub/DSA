65304b4e75d65c2a5216e77f69576da3e3ef60b6
Md Masud Sk <masudsk7074@gmail.com>
2026-08-28 16:05:09 +0530

    Added solution

--- 700160-bottom-view-of-binary-tree-GFG/700160-bottom-view-of-binary-tree.cpp
+++ 700160-bottom-view-of-binary-tree-GFG/700160-bottom-view-of-binary-tree.cpp
@@ -24,30 +24,26 @@ class Solution {
 		width(root->left, pos - 1, l, r);
 		width(root->right, pos + 1, l, r);
 	}
+	void Bview(Node *root, int index, int lev, vector<int> &ans, vector<int> &level) {
+		if (root == NULL) {
+			return;
+		}
+		if (level[index]<=lev) {
+			ans[index] = root->data;
+			level[index] = lev;
+		}
+		Bview(root->left, index - 1, lev+1, ans, level);
+		Bview(root->right, index + 1, lev+1, ans, level);
+	}
 	vector<int> bottomView(Node *root) {
 		// code here
 		int pos = 0, l = 0, r = 0;
 		width(root, pos, l, r);
 		vector<int> ans(r - l + 1);
-		queue<Node*>q;
-		queue<int> index;
-		q.push(root);
-		index.push(-l);
-		while (!q.empty()) {
-			Node *temp = q.front();
-			q.pop();
-			int i = index.front();
-			index.pop();
-			ans[i] = temp->data;
-			if (temp->left) {
-				q.push(temp->left);
-				index.push(i - 1);
-			}
-			if (temp->right) {
-				q.push(temp->right);
-				index.push(i + 1);
-			}
-		}
+		vector<int> level(r - l + 1, INT_MIN);
+		int index = -l;
+		int lev = 0;
+		Bview(root, index, lev, ans, level);
 		return ans;
 	}
 };
