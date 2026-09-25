d595de58c0a1702d6e8cdd5bb038decab9f61e62
Md Masud Sk <masudsk7074@gmail.com>
2026-09-25 15:03:30 +0530

    Added solution

--- 881705-building-heap-from-array-GFG/881705-building-heap-from-array.cpp
+++ 881705-building-heap-from-array-GFG/881705-building-heap-from-array.cpp
@@ -1,17 +1,29 @@
 class Solution {
 	public:
+	void heapify(vector<int> &arr, int index, int n) {
+		int largest = index;
+		int left = 2*index + 1;
+		int right = 2*index + 2;
+		if (left<n && arr[left]>arr[largest]) {
+			largest = left;
+		}
+		if (right<n && arr[right]>arr[largest]) {
+			largest = right;
+		} 
+		if (largest != index) {
+			swap(arr[largest], arr[index]);
+			heapify(arr, largest, n);
+		}
+	}
 	void buildHeap(vector<int>& arr) {
 		// code here
 		int size = arr.size();
-		for (int i =1;i<size; i++) {
-			int ci = i;
-			while (ci>0 && arr[(ci - 1)/2]<arr[ci]) {
-				swap(arr[(ci - 1)/2], arr[ci]);
-				ci = (ci - 1)/2;
-			}
+		for (int i = size/2 - 1; i >= 0; i--) {
+			heapify(arr, i, size);
 		}
 	}
 };
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
