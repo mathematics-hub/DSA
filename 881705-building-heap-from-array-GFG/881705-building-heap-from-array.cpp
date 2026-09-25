06308d7164ee507bad9ba96bdc5f43e3b1ae0111
Md Masud Sk <masudsk7074@gmail.com>
2026-09-25 13:33:57 +0530

    Added solution

---
+++ 881705-building-heap-from-array-GFG/881705-building-heap-from-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+	public:
+	void buildHeap(vector<int>& arr) {
+		// code here
+		int size = arr.size();
+		for (int i = 1; i<size; i++) {
+			int ci = i;
+			while (ci>0 && arr[(ci - 1)/2]<arr[ci]) {
+				swap(arr[(ci - 1)/2], arr[ci]);
+				ci = (ci - 1)/2;
+			}
+		}
+	}
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
