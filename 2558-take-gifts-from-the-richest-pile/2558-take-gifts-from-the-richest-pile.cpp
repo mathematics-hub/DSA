44fb326000e1e1be4216ab791a5a432509a0343a
Md Masud Sk <masudsk7074@gmail.com>
2026-09-25 19:38:32 +0530

    Time: 7 ms (11.24%), Space: 13 MB (86.32%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
-        for (int i = 1; i <= k; i++) {
-            int index = 0;
-            for (int j = 1; j < gifts.size(); j++) {
-                if (gifts[j] > gifts[index]) {
-                    index = j;
-                }
-            }
-            gifts[index] = sqrt(gifts[index]);
+        priority_queue<int> q;
+        for (int val : gifts) {
+            q.push(val);
         }
-        long long noOfGifts = 0;
-        for (int i = 0; i < gifts.size(); i++) {
-            noOfGifts += gifts[i];
+        while (k--) {
+            int temp = q.top();
+            q.pop();
+            q.push(sqrt(temp));
         }
-        return noOfGifts;
+        long long totalGifts = 0;
+        while (!q.empty()) {
+            totalGifts += q.top();
+            q.pop();
+        }
+        return totalGifts;
     }
 };
 
