794c4a1a10d70345ddbe4ae05b47b0b3379a3ab3
Md Masud Sk <masudsk7074@gmail.com>
2026-09-26 12:03:22 +0530

    Added solution

--- 705476-kth-largest-in-a-stream-GFG/705476-kth-largest-in-a-stream.cpp
+++ 705476-kth-largest-in-a-stream-GFG/705476-kth-largest-in-a-stream.cpp
@@ -3,18 +3,19 @@ class Solution {
 	vector<int> kthLargest(vector<int>& arr, int k) {
 		// code here
 		priority_queue<int, vector<int>, greater<int>> q;
-		vector<int> ans(k - 1, -1);
-		for (int i = 0; i<k; i++) {
+		vector<int> ans;
+		for (int i = 0; i<arr.size(); i++) {
 			q.push(arr[i]);
-		}
-		for (int i = k; i<arr.size(); i++) {
-			ans.push_back(q.top());
-			if (arr[i]>q.top()) {
+			if (q.size()>k) {
 				q.pop();
-				q.push(arr[i]);
+			}
+			if (q.size()<k) {
+				ans.push_back(-1);
+			}
+			if (q.size() == k) {
+				ans.push_back(q.top());
 			}
 		}
-		ans.push_back(q.top());
 		return ans;
 	}
 };
